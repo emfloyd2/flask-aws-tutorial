90c1b301b5c66e342c3b87d648593e915e481e3d
Mark Floyd <emfloyd2@gmail.com>
2017-09-05 00:35:29 -0700

    replacing secrets (the secret key isn't doing anything.. but good to encrypt it as good practice)

--- config.py
+++ config.py
@@ -6,12 +6,16 @@ import boto3
 from base64 import b64decode
 
 pw_encrypted = "AQICAHjtJ2LwO2/txZD+zYoGbFjgst2ppIoQMVmgkCF0iTNoNQECoe4yuQRk1965HyebnvbVAAAAajBoBgkqhkiG9w0BBwagWzBZAgEAMFQGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMc+ZchgqDOWRXISiBAgEQgCfqmzjnA3MSqx4/cDMzlBx+x6shkXrmWQqOBOITer2RILypJHIpkV0="
+
 user_encrypted = "AQICAHjtJ2LwO2/txZD+zYoGbFjgst2ppIoQMVmgkCF0iTNoNQFgMnILb/mIi8NPVGOp/rkoAAAAYzBhBgkqhkiG9w0BBwagVDBSAgEAME0GCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMjPPBaPFo+O2ZreKoAgEQgCCyw+rn4ZzJCvBhRG2/24/gXsKwx9OUBl3d72jrhrWZNA=="
+
+secret_key_encrypted = "AQICAHjtJ2LwO2/txZD+zYoGbFjgst2ppIoQMVmgkCF0iTNoNQEgDM4BcUCBfs8Ww+p1wLFPAAAAfzB9BgkqhkiG9w0BBwagcDBuAgEAMGkGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMTOxSIn3acgmakjr3AgEQgDxcs2VLWOTURUsgWO2huSdnZOOBpEZIhghfUvPZhSzfHaSV8n6spq7t4POKXC3DEEDLEc2nE2uBshxvBCQ="
 
 client = boto3.client('kms', region_name='us-east-2')
 
 pw = client.decrypt(CiphertextBlob=b64decode(pw_encrypted))['Plaintext']
 user = client.decrypt(CiphertextBlob=b64decode(user_encrypted))['Plaintext']
+secret_key = client.decrypt(CiphertextBlob=b64decode(secret_key_encrypted))['Plaintext']
 
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@piggy-dev-db.ckfc6vkfhmmc.us-east-2.rds.amazonaws.com:3306/piggydb'.format(user=user, password=pw)
 
@@ -21,4 +25,4 @@ SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@piggy-dev-db.ckfc6v
 SQLALCHEMY_POOL_RECYCLE = 3600
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
+SECRET_KEY = secret_key
