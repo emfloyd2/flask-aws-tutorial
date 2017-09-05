40198efd3f31708b7381bf093ec37e78c7093e62
Mark Floyd <emfloyd2@gmail.com>
2017-09-04 22:54:27 -0700

    Adding .gitignore file Adding encrypted user/pw credentials

--- config.py
+++ config.py
@@ -2,7 +2,18 @@
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb7ywmi.us-east-1.rds.amazonaws.com:3306/flaskdemo'
+import boto3
+from base64 import b64decode
+
+pw_encrypted = "AQICAHjtJ2LwO2/txZD+zYoGbFjgst2ppIoQMVmgkCF0iTNoNQECoe4yuQRk1965HyebnvbVAAAAajBoBgkqhkiG9w0BBwagWzBZAgEAMFQGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMc+ZchgqDOWRXISiBAgEQgCfqmzjnA3MSqx4/cDMzlBx+x6shkXrmWQqOBOITer2RILypJHIpkV0="
+user_encrypted = "AQICAHjtJ2LwO2/txZD+zYoGbFjgst2ppIoQMVmgkCF0iTNoNQFgMnILb/mIi8NPVGOp/rkoAAAAYzBhBgkqhkiG9w0BBwagVDBSAgEAME0GCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMjPPBaPFo+O2ZreKoAgEQgCCyw+rn4ZzJCvBhRG2/24/gXsKwx9OUBl3d72jrhrWZNA=="
+
+client = boto3.client('kms')
+
+pw = client.decrypt(CiphertextBlob=b64decode(pw_encrypted))['Plaintext']
+user = client.decrypt(CiphertextBlob=b64decode(user_encrypted))['Plaintext']
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@piggy-dev-db.ckfc6vkfhmmc.us-east-2.rds.amazonaws.com:3306/piggydb'.format(user=user, password=pw)
 
 # Uncomment the line below if you want to work with a local DB
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
